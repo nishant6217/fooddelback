47b30162d19ebe6b71f66b7f9c1df04007aa69e5
Nishant Kumar <me.nishant6217@gmail.com>
2021-08-09 22:56:40 +0530

    added atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/restro');
+mongoose.connect(
+  "mongodb+srv://nk6217:nk@217cluster0.28eqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
 const db = mongoose.connection;
 
-db.on('error' , console .error.bind(console,"error connecting mongodb"));
+db.on("error", console.error.bind(console, "error connecting mongodb"));
 
-db.once('open' , function(){
-    console.log('connected to database');
-
+db.once("open", function () {
+  console.log("connected to database");
 });
-
 
 module.exports = db;
